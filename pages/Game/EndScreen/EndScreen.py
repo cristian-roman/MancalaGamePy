3ccc9e7641b10bf646ad4f8a151ff854807b4bce
Cristian Roman <romancristianroman@yahoo.com>
2024-01-03 23:59:32 +0200

    end_screen buttons added

--- pages/Game/EndScreen/EndScreen.py
+++ pages/Game/EndScreen/EndScreen.py
@@ -1,37 +1,29 @@
-import os
 import sys
 
 import pygame
 
-from AppSettings import AppSettings
 from ViewTree import ViewTree
-from pages.ViewModel import ViewModel
 from pages.UI.Components.ButtonComponent import ButtonComponent
+from pages.ViewModel import ViewModel
 
 
 class EndScreen(ViewModel):
-    button_width = 100
-    button_height = 50
-    path_to_background_image = os.path.join('images', 'Game', 'EndScreen', 'end_screen.png')
+    button_width = 125
+    button_height = 62
 
     def __init__(self, window):
         super().__init__()
         self.window = window
-        self.image = pygame.image.load(self.path_to_background_image)
-        self.image = pygame.transform.scale(self.image, (AppSettings.board_height, AppSettings.board_width))
-        self.image_coordinates = (self.window.get_width() // 2 - AppSettings.board_width // 2,
-                                  self.window.get_height() // 2 - AppSettings.board_height // 2 + 50)
 
         self.play_again_button = ButtonComponent(self.window, "Play again",
-                                                  (self.window.get_width() // 2 - self.button_width,
-                                                   self.window.get_height() // 3 * 2),
-                                                  (self.button_width, self.button_height))
+                                                 (self.window.get_width() // 2 - self.button_width,
+                                                  self.window.get_height() // 3 * 2),
+                                                 (self.button_width, self.button_height))
         self.quit_button = ButtonComponent(self.window, "Quit",
-                                            (self.window.get_width() // 2, self.window.get_height() // 3 * 2),
-                                            (self.button_width, self.button_height))
+                                           (self.window.get_width() // 2, self.window.get_height() // 3 * 2),
+                                           (self.button_width, self.button_height))
 
     def _load_view(self):
-        self.window.blit(self.image, self.image_coordinates)
         self.play_again_button._draw()
         self.quit_button._draw()
         pygame.display.update()
@@ -43,10 +35,8 @@ class EndScreen(ViewModel):
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if self.play_again_button.button_rect.collidepoint(event.pos):
-                    from pages.Game.Game import Game
                     ViewTree.pop_view()
                     ViewTree.pop_view()
-                    ViewTree.push_view(Game(self.window))
                 elif self.quit_button.button_rect.collidepoint(event.pos):
                     pygame.quit()
                     sys.exit()
