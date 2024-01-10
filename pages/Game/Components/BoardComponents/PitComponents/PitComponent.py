d560cdc5a67f1dee3036a6ab92e02543d2933b96
Cristian Roman <romancristianroman@yahoo.com>
2024-01-10 23:16:06 +0200

    pep for Pit Component -- done

--- pages/Game/Components/BoardComponents/PitComponents/PitComponent.py
+++ pages/Game/Components/BoardComponents/PitComponents/PitComponent.py
@@ -13,13 +13,17 @@ class PitComponent(GameComponent):
     Class-level attributes:
         PIT_WIDTH (int): The width of the pit.
         PIT_HEIGHT (int): The height of the pit.
-        DEFAULT_STONE_NUMBER (int): The default number of stones contained in the pit.
-        DEFAULT_HIGHLIGHT_WIDTH (int): The default width of the ellipse that highlights the pit.
-        DEFAULT_LABEL_SIZE (int): The default size of the label that shows the number of stones contained in the pit.
+        DEFAULT_STONE_NUMBER (int):
+            The default number of stones contained in the pit.
+        DEFAULT_HIGHLIGHT_WIDTH (int):
+            The default width of the ellipse that highlights the pit.
+        DEFAULT_LABEL_SIZE (int):
+            The default size of the label
+            that shows the number of stones contained in the pit.
 
     Attributes:
         window (pygame.Surface): The window where the pit is drawn.
-        pit_coordinates (tuple): The coordinates of the pit on the table.
+        pit_coordinates (tuple): The coordinates of the pit.
         pit_index (int): The index of the pit on the table.
     """
     PIT_WIDTH = 102
@@ -29,28 +33,19 @@ class PitComponent(GameComponent):
     DEFAULT_LABEL_SIZE = 50
 
     def __init__(self, window, pit_coordinates, pit_index):
-        """This constructor initializes the attributes of the PitComponent.
+        """
+        The constructor initializing the attributes of the class.
 
-        Moreover, it initializes the following attributes:
-            is_highlighted (bool):  True if the pit is highlighted, False otherwise.
-                                    This attribute is used to control the highlighting of the pit.
+        It sets the window, the pit coordinates and the pit index
+        and initializes the stones contained in the pit.
+        It also initializes the label
+        that shows the number of stones contained in the pit.
 
-            stones (list):  The list of stones contained in the pit generated randomly.
-
-            label (LabelComponent): The label that shows the number of stones contained in the pit with its attributes:
-                                    position (tuple): The position of the label relative to pit X coordinate.
-                                    text (str): The text of the label. At this step DEFAULT_STONE_NUMBER.
-                                    size (int): The size of the label. The value is always DEFAULT_LABEL_SIZE.
-                                    color (tuple): The color of the text from label. It is always white.
-                                    has_background (bool): It is always True.
-                                    background_color (tuple):   The color of the background of the label.
-                                                                It is picked by querying the owner of the pit.
-                                    background_shape (str): The shape of the background of the label. It is always ellipse.
         Args:
             window (pygame.Surface): The window where the pit is drawn.
             pit_coordinates (tuple): The coordinates of the pit.
             pit_index (int): The index of the pit.
-            """
+        """
         self.window = window
         self.pit_coordinates = pit_coordinates
         self.pit_index = pit_index
@@ -70,7 +65,8 @@ class PitComponent(GameComponent):
             self.background_color = AppSettings.colors['orange_h6']
 
         self.label = LabelComponent(self.window,
-                                    position=(self.pit_coordinates[0] + self.PIT_WIDTH // 2, label_y),
+                                    position=(self.pit_coordinates[0] + self.PIT_WIDTH // 2,
+                                              label_y),
                                     text=str(self.DEFAULT_STONE_NUMBER),
                                     size=self.DEFAULT_LABEL_SIZE,
                                     color=AppSettings.colors['white'],
@@ -81,7 +77,42 @@ class PitComponent(GameComponent):
     def __init_stone(self):
         """Initialize the stone with a random number"""
         self.generator_number = (self.generator_number + 1) % 5 + 1
-        self.stones.append(StoneComponent(self.window, self.pit_coordinates, self.generator_number))
+        self.stones.append(StoneComponent(self.window,
+                                          self.pit_coordinates,
+                                          self.generator_number))
+
+    def is_mouse_hovering(self, mouse_position):
+        """
+        Check if the mouse is hovering the pit
+        by checking if the mouse position is in the zone defined by
+        pit coordinates and size.
+        It sets the highlight width to default value
+        for the cases when the mouse is not hovering the pit.
+        :param mouse_position:
+        :return: True if the mouse is hovering the pit, False otherwise.
+        """
+        self.highlight_width = self.DEFAULT_HIGHLIGHT_WIDTH
+        zone = pygame.Rect(self.pit_coordinates[0],
+                           self.pit_coordinates[1],
+                           self.PIT_WIDTH,
+                           self.PIT_HEIGHT)
+        return zone.collidepoint(mouse_position)
+
+    def treat_hovering(self):
+        """
+        Sets the highlight width to 10
+        and redraw the pit if the pit is not highlighted.
+        """
+        if not self.is_highlighted:
+            return
+        self.highlight_width = 10
+        self._draw()
+        pygame.display.update()
+
+    def add_stone(self, stone):
+        """Add a stone that was picked from another pit
+        to the current pit."""
+        self.stones.append(stone)
 
     def highlight(self):
         """Sets the is_highlighted attribute to True."""
@@ -91,27 +122,13 @@ class PitComponent(GameComponent):
         """Sets the is_highlighted attribute to False."""
         self.is_highlighted = False
 
-    def __draw_ellipse(self):
-        """
-        Draw the ellipse that highlights the pit in the zone defined by pit coordinates and size
-        with the highlight width and the background color defined in constructor
-        """
-        zone = pygame.Rect(self.pit_coordinates[0], self.pit_coordinates[1],
-                           self.PIT_WIDTH, self.PIT_HEIGHT)
-        pygame.draw.ellipse(self.window, self.background_color, zone, self.highlight_width)
-
-    def __erase_ellipse(self):
-        """Erase the ellipse that highlights the pit by drawing a transparent ellipse in the same zone."""
-        ellipse_surface = pygame.Surface((self.PIT_WIDTH, self.PIT_HEIGHT), pygame.SRCALPHA)
-        pygame.draw.ellipse(ellipse_surface,
-                            AppSettings.colors['no_color'],
-                            (0, 0, self.PIT_WIDTH, self.PIT_HEIGHT))
-        self.window.blit(ellipse_surface, self.pit_coordinates)
-
     def _draw(self):
         """
-        Draw the pit with its stones and the label that shows the number of stones contained in the pit.
-        If the pit is highlighted, draw the ellipse that highlights the pit.
+        Draw the pit with its stones
+        and the label that shows the number of stones
+        contained in the pit.
+        If the pit is highlighted,
+        draw the ellipse that highlights the pit.
         Else, erase the ellipse that highlights the pit.
         """
         self.label.set_text(str(len(self.stones)))
@@ -123,29 +140,29 @@ class PitComponent(GameComponent):
         else:
             self.__erase_ellipse()
 
-    def is_mouse_hovering(self, mouse_position):
+    def __draw_ellipse(self):
         """
-        Check if the mouse is hovering the pit by checking if the mouse position is in the zone defined by
-        pit coordinates and size.
-        It sets the highlight width to default value for the cases when the mouse is not hovering the pit.
-        :param mouse_position:
-        :return: True if the mouse is hovering the pit, False otherwise.
+        Draw the ellipse that highlights the pit
+        in the zone defined by pit coordinates and size
+        with the highlight width
+        and the background color defined in constructor
         """
-        self.highlight_width = self.DEFAULT_HIGHLIGHT_WIDTH
-        zone = pygame.Rect(self.pit_coordinates[0], self.pit_coordinates[1],
-                           self.PIT_WIDTH, self.PIT_HEIGHT)
-        return zone.collidepoint(mouse_position)
+        zone = pygame.Rect(self.pit_coordinates[0],
+                           self.pit_coordinates[1],
+                           self.PIT_WIDTH,
+                           self.PIT_HEIGHT)
+        pygame.draw.ellipse(self.window,
+                            self.background_color,
+                            zone,
+                            self.highlight_width)
 
-    def treat_hovering(self):
-        """
-        Sets the highlight width to 10 and redraw the pit if the pit is not highlighted.
-        """
-        if not self.is_highlighted:
-            return
-        self.highlight_width = 10
-        self._draw()
-        pygame.display.update()
-
-    def add_stone(self, stone):
-        """Add a stone that was picked from another pit to the current pit."""
-        self.stones.append(stone)
+    def __erase_ellipse(self):
+        """Erase the ellipse that highlights the pit
+        by drawing a transparent ellipse in the same zone."""
+        ellipse_surface = pygame.Surface((self.PIT_WIDTH,
+                                          self.PIT_HEIGHT),
+                                         pygame.SRCALPHA)
+        pygame.draw.ellipse(ellipse_surface,
+                            AppSettings.colors['no_color'],
+                            (0, 0, self.PIT_WIDTH, self.PIT_HEIGHT))
+        self.window.blit(ellipse_surface, self.pit_coordinates)
